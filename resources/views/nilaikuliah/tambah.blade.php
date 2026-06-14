3b994116d2a8468913706eb4273535ac263e3c4b
cleveeen-crypto <cleveeen@gmail.com>
2026-06-14 21:55:48 +0700

    SIAP EAS revisi

--- resources/views/nilaikuliah/tambah.blade.php
+++ resources/views/nilaikuliah/tambah.blade.php
@@ -10,16 +10,25 @@
         <p>
             <label>NRP</label><br>
             <input type="text" name="NRP" maxlength="10">
+            @error('KodeBarang')
+            <small style="color:red;">{{ $message }}</small>
+            @enderror
         </p>
 
         <p>
             <label>Nilai Angka</label><br>
             <input type="number" name="NilaiAngka" min="0" max="100">
+            @error('KodeBarang')
+            <small style="color:red;">{{ $message }}</small>
+            @enderror
         </p>
 
         <p>
             <label>SKS</label><br>
             <input type="number" name="SKS" min="1">
+            @error('KodeBarang')
+            <small style="color:red;">{{ $message }}</small>
+            @enderror
         </p>
 
         <button type="submit" class="btn btn-primary">Simpan</button>
